684dbbcfe3d1f115ea679fb30cf06aafaac9d802
evan-leigh <51345945+evan-leigh@users.noreply.github.com>
2022-09-09 15:51:15 -0400

    added fileicon to statusline

--- lua/statusline/components.lua
+++ lua/statusline/components.lua
@@ -4,20 +4,18 @@ local colors = require("palette").colors
 local mode = {
   value = function()
     local modes = {
-      ["n"] = "Normal",
+      ["n"] = "normal",
       ["i"] = "insert",
-      ["v"] = "Visual",
-      ["V"] = "V·Line",
-      [""] = "V·Block",
+      ["v"] = "visual",
+      ["V"] = "v·line",
+      [""] = "v·block",
       ["R"] = "replace",
       ["s"] = "select",
       ["c"] = "command",
-      ["t"] = "Terminal",
+      ["t"] = "terminal",
     }
-
     local current_mode = vim.api.nvim_get_mode().mode
-
-    vim.cmd("hi Mode guibg=" .. colors.purple .. " guifg=" .. colors.background_3)
+    vim.api.nvim_command("hi Mode guibg=" .. colors.accent .. " guifg=" .. colors.background_3)
     return "%#Mode# " .. string.format("%s", modes[current_mode]):upper() .. " %#Statusline#"
   end,
 }
@@ -31,21 +29,14 @@ local macro = {
   end,
 }
 
-local file = {
-  value = function()
-    local modified = vim.bo.modified and "[+] " or ""
-    return string.format("%s%s", modified, vim.fn.expand("%:p:t"))
-  end,
-}
+local line = { value = "%l:%L" }
 
 local search = {
   value = function()
     if vim.v.hlsearch == 0 then
       return ""
     end
-
     local count = vim.fn.searchcount({ maxcount = 999 })
-
     return string.format("%s [%s/%d]", vim.fn.getreg("/"), count.current, count.total)
   end,
   condition = function()
@@ -54,15 +45,23 @@ local search = {
   end,
 }
 
-local line = {
-  value = "%l:%L",
+local file = {
+  value = function()
+    local modified = vim.bo.modified and "[+] " or ""
+
+    local icon, color = require("nvim-web-devicons").get_icon_color(vim.fn.expand("%:e"), vim.bo.filetype)
+
+    color = color or ""
+    icon = icon or ""
+
+    vim.api.nvim_command("hi StatuslineFile guifg=" .. color .. " guibg=" .. colors.background_shaded_1)
+    icon = "%#StatuslineFile#" .. icon .. "%#Statusline#"
+
+    return string.format("%s%s %s", modified, icon, vim.fn.expand("%:p:t"))
+  end,
 }
 
-local status_ok, nvim_navic = pcall(require, "nvim-navic")
-
-if not status_ok then
-  return
-end
+local nvim_navic = require("nvim-navic")
 
 local navic = {
   value = function()
@@ -73,51 +72,44 @@ local navic = {
   end,
 }
 
-local right_align = {
-  value = "%=",
-}
+local right_align = { value = "%=" }
 
 local diagnostics = {
   value = function()
     local count = {}
-
-    local levels = {
-      errors = "Error",
-      warnings = "Warn",
-      info = "Info",
-      hints = "Hint",
-    }
-
+    local levels = { errors = "Error", warnings = "Warn", info = "Info", hints = "Hint" }
     for k, level in pairs(levels) do
       count[k] = vim.tbl_count(vim.diagnostic.get(0, { severity = level }))
     end
-
     local function format_diagnostic(icon, status)
       return count[status] ~= 0 and string.format(icon .. " %s", count[status]) or ""
     end
-
-    local diagnostics = {
+    return table.concat({
       format_diagnostic("  ", "errors"),
       format_diagnostic(" 𥉉", "warnings"),
-      format_diagnostic(" 𥉉", "hints"),
-      format_diagnostic("  ", "info"),
-    }
-
-    return unpack(diagnostics)
+      format_diagnostic("  ", "hints"),
+      format_diagnostic("  ", "info"),
+    })
   end,
 }
+
+local root = {
+  value = function()
+    local root = vim.fn.getcwd()
+    local workspace = root:sub(root:find("[^/]*$"))
+    return workspace
+  end,
+}
+
 local git = {
   value = function()
     local git_info = vim.b.gitsigns_status_dict
-
     if not git_info or git_info.head == "" then
       return ""
     end
-
     local function format_git(icon, status)
       return status ~= 0 and string.format(" " .. icon .. "%s", status) or ""
     end
-
     return table.concat({
       git_info.head,
       format_git("+", git_info.added),
@@ -130,11 +122,12 @@ local git = {
 return {
   [1] = mode,
   [2] = macro,
-  [3] = file,
+  [3] = line,
   [4] = search,
-  [5] = line,
+  [5] = file,
   [6] = navic,
   [7] = right_align,
   [8] = diagnostics,
-  [9] = git,
+  [9] = root,
+  [10] = git,
 }
